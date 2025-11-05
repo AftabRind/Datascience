a8e51324e1d8f1cd6bab733ebd65dae6a4685331
Aftab Ali <76578660+AftabRind@users.noreply.github.com>
2025-11-05 11:07:52 +0800

    Created using Colab

---
+++ Multi-Agent MHADQN.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPlvfwbRj2RxoDknor5sl4s",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/AftabRind/Datascience/blob/main/Multi-Agent%20MHADQN.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "HbX9aPMB-LuU"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
